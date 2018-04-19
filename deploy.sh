c7fa0feb8d33f54693722ce3265f2c64881c7866
Eoin McCarthy <em1081@my.bristol.ac.uk>
2018-04-19 16:19:27 +0300

    fixes deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 set -e
 echo bulid client ...
+cd notes-client
 npm run build
+cd ..
 git checkout -b gh-pages
-git add -f build
+git add -f notes-client/build
 git commit -m "deploy to gh-pages"
 echo push to remote gh-pages
-git push origin `git subtree split --prefix build`:gh-pages --force
+git push origin `git subtree split --prefix notes-client/build`:gh-pages --force
 git checkout master
 git branch -D gh-pages
 echo done

ea9fea32863e60ee41fc3267358489cab1f2f04e
Eoin McCarthy <em1081@my.bristol.ac.uk>
2018-12-08 15:04:54 +0200

    removes other markdown parser tests

--- packages/editor/vnode_creators/vnode_remarkable.js
+++
@@ -1,9 +0,0 @@
-const Remarkable = require('remarkable');
-const md = new Remarkable();
-
-const vnode = ast => {
-  console.log(ast)
-  return ast
-}
-
-export default text => vnode(md.parse(text, {}))
